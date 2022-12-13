f13a850c597eeec82cafd65e136bf25bb9e73229
kazuya1114 <82861954+kazuya1114@users.noreply.github.com>
2022-12-13 16:54:55 +0900

    コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'MyApp.dart';
 void main() {
   /// MyAppウィジェットをriverpodで管理する
   /// riverpod：複数の画面で利用する変数等を保持、状態の管理を行う(保持した値はどこでも呼び出せるため、
-  /// 引数で値の受け渡しをしないでいい)
+  /// 引数で値の受け渡しをしないでいい)。riverpodはproviderの欠点をなくすために作られた後天的なライブラリ
+  /// riverpodとproviderの違いは下記URL参照
+  /// https://zenn.dev/kikuchy/articles/5feec4fa7444c6
   runApp(ProviderScope(child: MyApp()));
 }
