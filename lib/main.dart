87b92fe01841a22c591c372bbb437fd8a1c237d5
kazuya1114 <82861954+kazuya1114@users.noreply.github.com>
2022-12-14 11:11:40 +0900

    コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   /// MyAppウィジェットをriverpodで管理する
   /// riverpod：複数の画面で利用する変数等を保持、状態の管理を行う(保持した値はどこでも呼び出せるため、
   /// 引数で値の受け渡しをしないでいい)。riverpodはproviderの欠点をなくすために作られた後天的なライブラリ
-  /// riverpodとproviderの違いは下記URL参照
-  /// https://zenn.dev/kikuchy/articles/5feec4fa7444c6
+  /// riverpodとproviderの違いは下記URL参考：https://zenn.dev/kikuchy/articles/5feec4fa7444c6
+  /// MVVM + Repositoryパターン参考：https://wasabeef.medium.com/flutter-%E3%82%92-mvvm-%E3%81%A7%E5%AE%9F%E8%A3%85%E3%81%99%E3%82%8B-861c5dbcc565
   runApp(ProviderScope(child: MyApp()));
 }
